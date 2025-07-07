1f3cd8b0113c00b68b081123a0a614be36d9371b
shihabict <shihabict.ict@gmail.com>
2025-07-06 22:51:07 -0500

    Spawn vehicle3

--- research/spawn_cars.py
+++ research/spawn_cars.py
@@ -3,10 +3,14 @@ import random
 import cv2
 import numpy as np
 
+from research.load_custom_path import world
+
 # connect to the simulator
 client = carla.Client('localhost', 2000)
 # load predefine town from carla
 client.load_world('Town05')
+
+world = client.get_world()
 
 # extract spawn points
 spawn_points = world.get_map().get_spawn_points()
