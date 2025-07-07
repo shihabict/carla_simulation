dce78d9435cb2eb5788c93cce8c7712c300f3f41
shihabict <shihabict.ict@gmail.com>
2025-07-06 22:48:14 -0500

    Spawn vehicle

--- research/spawn_cars.py
+++ research/spawn_cars.py
@@ -3,8 +3,16 @@ import random
 import cv2
 import numpy as np
 
+from research.spectator_view import spawn_points, world, start_point, vehicle
 
 # connect to the simulator
 client = carla.Client('localhost', 2000)
 # load predefine town from carla
 client.load_world('Town05')
+
+# extract spawn points
+spawn_points = world.get_map().get_spawn_points()
+start_point = spawn_points[0]
+vehicle_bp = world.get_blueprint_library().filter('*mini*')
+
+vehicle = world.try_spawn_actor(vehicle_bp, start_point)
