9bf39f0d3375e7b28f78fc63bc2477ad93aef42d
shihabict <shihabict.ict@gmail.com>
2025-07-06 22:57:50 -0500

    Added camera and n numbers of vehicles

--- research/spawn_cars.py
+++ research/spawn_cars.py
@@ -6,13 +6,44 @@ import numpy as np
 # connect to the simulator
 client = carla.Client('localhost', 2000)
 # load predefine town from carla
+world = client.get_world()
 client.load_world('Town05')
 
-world = client.get_world()
+# Retrieve the spectator object
+spectator = world.get_spectator()
 
-# extract spawn points
+# Get the location and rotation of the spectator through its transform
+transform = spectator.get_transform()
+
+location = transform.location
+rotation = transform.rotation
+
+# Set the spectator with an empty transform
+spectator.set_transform(carla.Transform())
+# This will set the spectator at the origin of the map, with 0 degrees
+# pitch, yaw and roll - a good way to orient yourself in the map
+
+# Get the blueprint library and filter for the vehicle blueprints
+vehicle_blueprints = world.get_blueprint_library().filter('*vehicle*')
+
+# Get the map's spawn points
 spawn_points = world.get_map().get_spawn_points()
-start_point = spawn_points[0]
-vehicle_bp = world.get_blueprint_library().filter('*mini*')
 
-vehicle = world.try_spawn_actor(vehicle_bp, start_point)
+# Spawn 50 vehicles randomly distributed throughout the map
+# for each spawn point, we choose a random vehicle from the blueprint library
+for i in range(0,50):
+    world.try_spawn_actor(random.choice(vehicle_blueprints), random.choice(spawn_points))
+
+ego_vehicle = world.spawn_actor(random.choice(vehicle_blueprints), random.choice(spawn_points))
+
+# Create a transform to place the camera on top of the vehicle
+camera_init_trans = carla.Transform(carla.Location(z=1.5))
+
+# We create the camera through a blueprint that defines its properties
+camera_bp = world.get_blueprint_library().find('sensor.camera.rgb')
+
+# We spawn the camera and attach it to our ego vehicle
+camera = world.spawn_actor(camera_bp, camera_init_trans, attach_to=ego_vehicle)
+
+
+
