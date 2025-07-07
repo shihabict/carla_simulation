b17a08acd8b425228513e8ac525e99b2f11f9971
shihabict <shihabict.ict@gmail.com>
2025-07-06 22:50:14 -0500

    Spawn vehicle2

--- research/spawn_cars.py
+++ research/spawn_cars.py
@@ -3,8 +3,6 @@ import random
 import cv2
 import numpy as np
 
-from research.spectator_view import spawn_points, world, start_point, vehicle
-
 # connect to the simulator
 client = carla.Client('localhost', 2000)
 # load predefine town from carla
